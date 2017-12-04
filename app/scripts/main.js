259b88b96b34cabc00204d73a7ebe50135396197
root <root@ip-172-31-14-128.eu-west-2.compute.internal>
2017-12-04 13:41:55 +0000

    add http protocol

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -108,7 +108,7 @@ if (IS_CX) {
 }
 var app = angular.module('mewApp', ['pascalprecht.translate', 'ngSanitize','ngAnimate']);
 app.config(['$compileProvider', function($compileProvider) {
-  $compileProvider.aHrefSanitizationWhitelist(/^\s*(|blob|https|mailto):/);
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(|blob|https|http|mailto):/);
 }]);
 app.config(['$translateProvider', function($translateProvider) {
   $translateProvider.useMissingTranslationHandlerLog();
